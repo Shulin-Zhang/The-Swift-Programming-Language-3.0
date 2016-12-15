02abbef4d8adfe96b2a4695d739b180430095473
Coderzhangsl <zhangsl@zhangsldeMac-mini.local>
2016-12-15 16:23:54 +0800

    Initialization

---
+++ 14_Initialization.playground/Contents.swift
@@ -0,0 +1,357 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+struct Fahrenheit {
+    var temperature: Double
+    init() {
+        temperature = 32.0
+    }
+}
+var f = Fahrenheit()
+print("The default temperature is \(f.temperature) Fahreheit")
+
+struct Fahrenheit2 {
+    var temperature = 32.0
+}
+
+struct Celsius {
+    var temperatureInCelsius: Double
+    init(fromFahrenheit fahrenheit: Double) {
+        temperatureInCelsius = (fahrenheit - 32.0) / 1.8
+    }
+    init(fromKelvin kelvin: Double) {
+        temperatureInCelsius = kelvin - 273.15
+    }
+}
+
+let boilingPointOfWater = Celsius(fromFahrenheit: 212.0)
+let freezingPointOfWater = Celsius(fromKelvin: 273.15)
+
+struct Color {
+    let red, green, blue: Double
+    init(red: Double, green: Double, blue: Double) {
+        self.red = red
+        self.green = green
+        self.blue = blue
+    }
+    init(white: Double) {
+        red = white
+        green = white
+        blue = white
+    }
+}
+
+let magenta = Color(red: 1.0, green: 0.0, blue: 1.0)
+let halfGray = Color(white: 0.5)
+
+struct Celsius2 {
+    var temperatureInCelsius: Double
+    init(fromFahrenheit fahrenheit: Double) {
+        temperatureInCelsius = (fahrenheit - 32.0) / 1.8
+    }
+    init(fromKelvin kelvin: Double) {
+        temperatureInCelsius = kelvin - 273.15
+    }
+    init(_ celsius: Double) {
+        temperatureInCelsius = celsius
+    }
+}
+
+let bodyTemperature = Celsius2(37.0)
+
+class SurveyQuestion {
+    var text: String
+    var response: String?
+    init(text: String) {
+        self.text = text
+    }
+    
+    func ask() {
+        print(text)
+    }
+}
+
+let cheeseQuestion = SurveyQuestion(text: "Do you like cheese?")
+cheeseQuestion.ask()
+cheeseQuestion.response = "Yes, I do like cheese"
+
+class SurveyQuestion2 {
+    let text: String
+    var response: String?
+    init(text: String) {
+        self.text = text
+    }
+    func ask() {
+        print(text)
+    }
+}
+
+let beetsQuestion = SurveyQuestion2(text: "How about beets")
+beetsQuestion.ask()
+beetsQuestion.response = "I also like beets. (But not with cheese)"
+
+class ShoppingListItem {
+    var name: String?
+    var quantity = 1
+    var purchased = false
+}
+
+var item = ShoppingListItem()
+
+struct Size {
+    var width = 0.0, height = 0.0
+}
+let twoByTwo = Size(width: 2.0, height: 2.0)
+
+struct Size2 {
+    var width = 0.0, height = 0.0
+}
+
+struct Point {
+    var x = 0.0, y = 0.0
+}
+
+struct Rect {
+    var origin = Point()
+    var size = Size2()
+    init() {}
+    init(origin: Point, size: Size2) {
+        self.origin = origin
+        self.size = size
+    }
+    init(center: Point, size: Size2) {
+        let originX = center.x - (size.width / 2)
+        let originY = center.y - (size.height / 2)
+        self.init(origin: Point(x: originX, y: originY), size: size)
+    }
+}
+
+class Vehicle {
+    var numberOfWheels = 0
+    var description: String {
+        return "\(numberOfWheels) wheels"
+    }
+}
+
+let vehicle = Vehicle()
+print("Vehicle: \(vehicle.description)")
+
+class Bicycle: Vehicle {
+    override init() {
+        super.init()
+        numberOfWheels = 2
+    }
+}
+
+let bicycle = Bicycle()
+print("Bicycle: \(bicycle.description)")
+
+class Food {
+    var name: String
+    init (name: String) {
+        self.name = name
+    }
+    convenience init() {
+        self.init(name: "[Unnamed]")
+    }
+}
+
+let namedMeat = Food(name: "Bacon")
+let mysteryMeat = Food()
+
+class RecipeIngredient: Food {
+    var quantity: Int
+    init(name: String, quantity: Int) {
+        self.quantity = quantity
+        super.init(name: name)
+    }
+    override convenience init(name: String) {
+        self.init(name: name, quantity: 1)
+    }
+}
+
+let oneMysteryItem = RecipeIngredient()
+let oneBacon = RecipeIngredient(name: "Bacon")
+let sixEggs = RecipeIngredient(name: "Eggs", quantity: 6)
+
+class ShoppingListItem2: RecipeIngredient {
+    var purchased = false
+    var descirption: String {
+        var output = "\(quantity) x \(name)"
+        output += purchased ? "✅" : "❌"
+        return output
+    }
+}
+
+var breakfastList = [
+    ShoppingListItem2(),
+    ShoppingListItem2(name: "Bacon"),
+    ShoppingListItem2(name: "Eggs", quantity: 6)
+]
+breakfastList[0].name = "Orange juice"
+breakfastList[0].purchased = true
+for item in breakfastList {
+    print(item.descirption)
+}
+
+struct Animal {
+    let species: String
+    init?(species: String) {
+        if species.isEmpty { return nil }
+        self.species = species
+    }
+}
+
+let someCreature = Animal(species: "Giraffe")
+if let giraffe = someCreature {
+    print()
+}
+
+let anonymousCreature = Animal(species: "");
+
+if anonymousCreature == nil {
+    print()
+}
+
+enum TemperatureUnit {
+    case Kelvin, Celsius, Fahrenheit
+    init?(symbol: Character) {
+        switch symbol {
+        case "K":
+            self = .Kelvin
+        case "C":
+            self = .Celsius
+        case "F":
+            self = .Fahrenheit
+        default:
+            return nil
+        }
+    }
+}
+
+let fanrenheitUnit = TemperatureUnit(symbol: "F")
+if fanrenheitUnit != nil {
+    print()
+}
+
+let unknowUnit = TemperatureUnit(symbol: "X")
+if unknowUnit == nil {
+    print()
+}
+
+enum TemperatureUnit2: Character {
+    case Kelvin = "K", Celsius = "C", Fahrenheit = "F"
+}
+
+let fahrenheitUnit2 = TemperatureUnit2(rawValue: "F")
+let unknownUnit2 = TemperatureUnit2(rawValue: "X")
+
+class Product {
+    let name: String
+    init?(name: String) {
+        if name.isEmpty { return nil }
+        self.name = name
+    }
+}
+
+class CartItem: Product {
+    let quantity: Int
+    init?(name: String, quantity: Int) {
+        if quantity < 1 { return nil }
+        self.quantity = quantity
+        super.init(name: name) 
+    }
+}
+
+if let twoSocks = CartItem(name: "sock", quantity: 2) {
+    
+}
+
+if let zeroShirts = CartItem(name: "shirt", quantity: 0) {
+    
+}
+
+if let oneUnnamed = CartItem(name: "", quantity: 1) {
+    
+}
+
+class Document {
+    var name: String?
+    init() {}
+    init?(name: String) {
+        if name.isEmpty { return nil }
+        self.name = name
+    }
+}
+
+class AutomaticallyNamedDocument: Document {
+    override init() {
+        super.init()
+        self.name = "[Untitled]"
+    }
+    override init(name: String) {
+        super.init()
+        if name.isEmpty {
+            self.name = "[Unitiled]"
+        } else {
+            self.name = name
+        }
+    }
+}
+
+class UntitledDocument: Document {
+    override init() {
+        super.init(name: "[Untitled]")!
+    }
+}
+
+class SomeClass {
+    required init() {
+        
+    }
+}
+
+class SomeSubclass: SomeClass {
+    required init() {
+        
+    }
+}
+
+class SomeClass2 {
+    let someProperty: Int = {
+        let someValue: Int = 2 * 100
+        return someValue
+    }()
+}
+
+class SomeClass3 {
+    lazy var button: UIButton = {
+       let button = UIButton(type: .custom)
+        button.backgroundColor = UIColor.red
+        button.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        
+        return button
+    }()
+}
+
+struct Checkerboard {
+    let boardColors: [Bool] = {
+       var temporaryBoard = [Bool]()
+        var isBlack = false
+        for i in 1...8 {
+            for j in 1...8 {
+                temporaryBoard.append(isBlack)
+                isBlack = !isBlack
+            }
+            isBlack = !isBlack
+        }
+        return temporaryBoard
+    }()
+    
+    func squareIdBlackAtRow(row: Int, column: Int) -> Bool {
+        return boardColors[(row * 8) + column]
+    }
+}
+
+
